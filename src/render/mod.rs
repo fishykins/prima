f39a02d78f8c314c6f30f2e6ea49b9afe84b1937
Fishykins <caspar.m.green@gmail.com>
2020-09-30 23:49:16 +0100

    made draw public

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,14 +1,16 @@
 mod draw_line;
 mod draw_circle;
-mod draw;
+
+pub mod draw;
 
 pub use draw_line::draw_line;
 pub use draw_line::draw_line_segment;
 pub use draw_circle::draw_circle;
+pub use draw::Draw;
 
 pub use image::{RgbImage, ImageBuffer, Pixel, Rgb as RgbRaw};
 
-/// reverses the y value!
+/// reverses the y value! A better alternative to flipping the while image later on
 pub fn paint_pixel(image: &mut RgbImage, x: u32, y: u32, rgb: RgbRaw<u8>) {
     //println!("Painting [{},{}]", x, y);
     if x < image.width() && y < image.height() && y > 0 && x > 0 {
