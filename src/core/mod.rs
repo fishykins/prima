eba94498535df5125842dfa841671d8ef10d98ab
Fishy <caspar.m.green@gmail.com>
2022-02-19 01:53:17 +0000

    Added plane mapping for grids

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,7 @@
 mod axis;
 mod index_type;
 mod ord_num;
+mod plane;
 mod transverse;
 
 /// A few useful mathematical functions.
@@ -9,4 +10,5 @@ pub mod maths;
 pub use axis::Axis;
 pub use index_type::{DefaultIx, IndexType};
 pub use ord_num::OrdNum;
+pub use plane::{Plane2D, Plane3D};
 pub use transverse::Transverse;
--- src/graphs/grid.rs
+++ src/graphs/grid.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
 
 use glam::Vec2;
 
-use crate::geom::Coord;
+use crate::{core::Plane2D, geom::Coord};
 
 /// A grid based on a 2D coordinate system.
 #[derive(Clone)]
@@ -11,26 +12,32 @@ pub struct Grid<T> {
     /// The number of cells in the y plane
     height: u32,
     /// The size of each cell (which is assumed to be a square)
-    pub cell_size: f32,
+    cell_size: f32,
     /// World anchor point.
-    pub anchor: Vec2,
+    anchor: Vec2,
     /// Cell data
-    pub cells: Vec<Option<T>>,
+    cells: HashMap<Coord, T>,
+    /// A boolean map of the grid, using x as the outer index and y as the inner index.
+    x_map: Vec<Vec<bool>>,
+    /// A boolean map of the grid, using y as the outer index and x as the inner index.
+    y_map: Vec<Vec<bool>>,
 }
 
-impl<T> Grid<T> where T: Clone {
+impl<T> Grid<T>
+where
+    T: Clone,
+{
     /// Makes a new grid.
     pub fn new(width: u32, height: u32, cell_size: f32, anchor: Vec2) -> Grid<T> {
-        let mut grid = Grid {
+        Grid {
             width,
             height,
             cell_size,
             anchor,
-            cells: Vec::with_capacity((width * height) as usize),
-        };
-
-        grid.cells.resize((width * height) as usize, None);
-        grid
+            cells: HashMap::with_capacity((width * height) as usize),
+            x_map: vec![vec![false; height as usize]; width as usize],
+            y_map: vec![vec![false; width as usize]; height as usize],
+        }
     }
 
     /// Clamps a given coordinate to the grid bounds.
@@ -70,38 +77,48 @@ impl<T> Grid<T> where T: Clone {
 
     /// Adds a cell to the grid.
     pub fn insert_cell(&mut self, coord: Coord, cell: T) {
-        let index = self.index(coord);
-        self.cells[index] = Some(cell);
+        self.cells.insert(coord, cell);
+        self.x_map[coord.x as usize][coord.y as usize] = true;
+        self.y_map[coord.y as usize][coord.x as usize] = true;
     }
 
     /// Gets the cell data at given coordinate.
     pub fn cell_data(&self, coord: Coord) -> Option<&T> {
-        let i = self.index(coord);
-        if let Some(cell) = self.cells.get(i) {
-            cell.as_ref()
-        } else {
-            None
-        }
+        self.cells.get(&self.clamp_coord(coord))
     }
 
     /// Gets the cell data at given coordinate.
     pub fn cell_data_mut(&mut self, coord: Coord) -> Option<&mut T> {
-        let i = self.index(coord);
-        if let Some(cell) = self.cells.get_mut(i) {
-            cell.as_mut()
-        } else {
-            None
-        }
+        self.cells.get_mut(&self.clamp_coord(coord))
     }
 
     /// Removes a cell at given coordinate.
     pub fn remove_cell(&mut self, coord: Coord) {
-        let index = self.index(coord);
-        self.cells[index] = None;
+        self.cells.remove(&self.clamp_coord(coord));
+        self.x_map[coord.x as usize][coord.y as usize] = false;
+        self.y_map[coord.y as usize][coord.x as usize] = false;
     }
 
-    /// Computes the index of the given coordinate.
-    fn index(&self, coord: Coord) -> usize {
-        (coord.y * self.width + coord.x) as usize
+    /// Gets all cells in the grid.
+    pub fn cells(&self) -> Vec<(Coord, &T)> {
+        self.cells.iter().map(|(c, t)| (*c, t)).collect()
+    }
+
+    /// Gets all cells lying on the given plane.
+    pub fn cells_on_plane(&self, plane: Plane2D, value: u32) -> Vec<Coord> {
+        match plane {
+            Plane2D::X => self.x_map[value as usize]
+                .iter()
+                .enumerate()
+                .filter(|(_, &b)| b)
+                .map(|(y, _)| Coord::new(value, y as u32))
+                .collect(),
+            Plane2D::Y => self.y_map[value as usize]
+                .iter()
+                .enumerate()
+                .filter(|(_, &b)| b)
+                .map(|(x, _)| Coord::new(x as u32, value))
+                .collect(),
+        }
     }
 }
