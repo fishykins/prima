f27fee1881770e5757f9e8ed5037b5071cedf03e
Fishy <caspar.m.green@gmail.com>
2022-04-22 01:03:34 +0100

    implimented circle/line

--- src/interactions/circle_lines.rs
+++ src/interactions/circle_lines.rs
@@ -1,37 +1,56 @@
-use crate::{Circle, Intersect, Line, PrimaFloat};
+use crate::{Circle, Collision, FastDistance, Interact, Intersect, Line, Point, PrimaFloat, Shape};
 
-// impl<N> Collide<N, Line2<N>> for Circle<N> where N: PrimaFloat {
-//     /// Returns the closest point on the line to the circle.
-//     fn collision(&self, line: &Line2<N>) -> Option<Collision<N>> {
-//         let start_inside = self.contains_point(&line.start);
-//         let end_inside = self.contains_point(&line.end);
-//         if start_inside || end_inside {
-//             return None;
-//         }
+impl<N> Interact<N, Line<N>> for Circle<N>
+where
+    N: PrimaFloat,
+{
+    /// Returns the closest point on the line to the circle.
+    fn collision(&self, line: &Line<N>) -> Option<Collision<N>> {
+        let start_inside = self.contains_point(&line.start);
+        let end_inside = self.contains_point(&line.end);
+        if start_inside || end_inside {
+            return None;
+        }
 
-//         let length = line.length();
-//         let dot = (((self.center.x - line.start.x) * (line.end.x - line.start.x))
-//             + ((self.center.y - line.start.y) * (line.end.y - line.start.y)))
-//             / length.powi(2);
+        let closest = self.nearest_extent(line);
+        if !line.contains_point(closest) {
+            return None;
+        }
 
-//         let closest_x = line.start.x + dot * (line.end.x - line.start.x);
-//         let closest_y = line.start.y + dot * (line.end.y - line.start.y);
-//         let closest = Point2::new(closest_x, closest_y);
-//         if !line.contains_point(closest) {
-//             return None;
-//         }
-//         if self.center.distance_squared(&closest) <= self.radius * self.radius {
-//             return Some(closest);
-//         }
-//         None
-//     }
-// }
+        let dist_squared = self.center.distance_squared(&closest);
+
+        if dist_squared <= self.radius * self.radius {
+            return Some(Collision::<N> {
+                penetration: self.radius - dist_squared.sqrt(),
+                normal: closest.vector(self.center).normalize(),
+            });
+        }
+        None
+    }
+
+    fn nearest_extent(&self, line: &Line<N>) -> Point<N> {
+        let length = line.length();
+        let dot = (((self.center.x - line.start.x) * (line.end.x - line.start.x))
+            + ((self.center.y - line.start.y) * (line.end.y - line.start.y)))
+            / length.powi(2);
+
+        let closest_x = line.start.x + dot * (line.end.x - line.start.x);
+        let closest_y = line.start.y + dot * (line.end.y - line.start.y);
+        Point::new(closest_x, closest_y)
+    }
+}
 
 impl<N> Intersect<Line<N>> for Circle<N>
 where
     N: PrimaFloat,
 {
-    fn intersecting(&self, _line: &Line<N>) -> bool {
-        todo!()
+    fn intersecting(&self, line: &Line<N>) -> bool {
+        let start_inside = self.contains_point(&line.start);
+        let end_inside = self.contains_point(&line.end);
+        if start_inside || end_inside {
+            return true;
+        }
+        let closest = self.nearest_extent(line);
+        self.contains_point(&closest)
     }
 }
